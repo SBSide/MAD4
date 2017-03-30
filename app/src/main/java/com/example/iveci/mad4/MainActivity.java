4aab5738cc7c92b53768d8a329d7a1e7b1fbc954
SBSide <ivecis@live.co.kr>
2017-03-30 15:17:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/iveci/mad4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iveci.mad4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("Pasta 예약시스템");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

309c73ed2b67d1a32579a82cd5b376f2158a25bb
Side <ivecis@live.co.kr>
2017-04-05 13:43:17 +0900

    hw4-1

--- app/src/main/java/com/example/iveci/mad4/Fragment1.java
+++ app/src/main/java/com/example/iveci/mad4/Fragment1.java
@@ -10,11 +10,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static android.content.ContentValues.TAG;
 
@@ -25,28 +29,59 @@ import static android.content.ContentValues.TAG;
 public class Fragment1 extends Fragment {
     final Table[] LIST_MENU = {new Table("사과 Table"),new Table("포도 Table"),new Table("키위 Table"),new Table("자몽 Table")};
     Button b1,b2,b3;
-    TextView t1,t2;
+    TextView t1,t2,t3,t4,t5,t6,t7,t8;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View fragv1 = inflater.inflate(R.layout.fragment1,null);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.support_simple_spinner_dropdown_item, LIST_MENU) ;
-        ListView listview = (ListView) fragv1.findViewById(R.id.list01) ;
-        listview.setAdapter(adapter);
         b1 = (Button) fragv1.findViewById(R.id.neworder);
         b2 = (Button) fragv1.findViewById(R.id.correct);
         b3 = (Button) fragv1.findViewById(R.id.initia);
-        t1 = (TextView) fragv1.findViewById(R.id.autotab);
-        t2 = (TextView) fragv1.findViewById(R.id.autotime);
+        t3 = (TextView) fragv1.findViewById(R.id.tablename);
+        t4 = (TextView) fragv1.findViewById(R.id.tenter);
+        t5 = (TextView) fragv1.findViewById(R.id.tspag);
+        t6 = (TextView) fragv1.findViewById(R.id.tpizza);
+        t7 = (TextView) fragv1.findViewById(R.id.tmember);
+        t8 = (TextView) fragv1.findViewById(R.id.tprice);
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.support_simple_spinner_dropdown_item, LIST_MENU) ;
+        ListView listview = (ListView) fragv1.findViewById(R.id.list01) ;
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (LIST_MENU[position].isEmpty()){
+                    Log.d(TAG,"EMPTY");
+                    t3.setText(parent.getItemAtPosition(position).toString());
+                    Toast.makeText(getActivity(),
+                            "빈 테이블입니다.",Toast.LENGTH_SHORT)
+                            .show();
+                }
+                else {
+                    int spa = LIST_MENU[position].getNumspag();
+                    int piz = LIST_MENU[position].getNumpizza();
+                    t4.setText(LIST_MENU[position].getEnterDate());
+                    t5.setText(spa);
+                    t6.setText(piz);
+                    t7.setText(LIST_MENU[position].getMember() ? "VIP멤버쉽" : "기본멤버쉽");
+                    t8.setText((spa * 10000 + piz * 12000) + "원");
+                }
+            }
+        });
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 View view = View.inflate(getActivity(), R.layout.order, null);
                 AlertDialog.Builder order = new AlertDialog.Builder(getActivity());
-                /*for (int i = 0; i < LIST_MENU.length; i++){
-                    if (LIST_MENU[i].isEmpty())
-                        t1.setText(LIST_MENU[i].getTablename());
-                }*/
+                t1 = (TextView) view.findViewById(R.id.autotab);
+                t2 = (TextView) view.findViewById(R.id.autotime);
+                for (int i = 0; i < LIST_MENU.length; i++){
+                    if (LIST_MENU[i].isEmpty()) {
+                        t1.setText(" " + LIST_MENU[i].getTablename());
+                        break;
+                    }
+                }
+                t2.setText(" " + new SimpleDateFormat("yyyyMMdd HH:mm")
+                        .format(new Date(System.currentTimeMillis())));
                 order.setTitle("새 주문")
                         .setView(view)
                         .setNegativeButton("취소", new DialogInterface.OnClickListener() {
@@ -70,7 +105,7 @@ public class Fragment1 extends Fragment {
             public void onClick(View v) {
                 View view = View.inflate(getActivity(), R.layout.order, null);
                 AlertDialog.Builder order = new AlertDialog.Builder(getActivity());
-                order.setTitle("새 주문")
+                order.setTitle("주문 수정")
                         .setView(view)
                         .setNegativeButton("취소", new DialogInterface.OnClickListener() {
                             @Override
@@ -99,4 +134,5 @@ public class Fragment1 extends Fragment {
 
         return fragv1;
     }
+
 }
